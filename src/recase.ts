b2269548238e87ec15689d62bc307d59b459bd40
Devin Riegle <riegledevin@gmail.com>
2019-10-02 23:10:18 -0400

    Add Recase class with methods

--- src/recase.ts
+++ src/recase.ts
@@ -1,6 +1,60 @@
-// Import here Polyfills if needed. Recommended core-js (npm i -D core-js)
-  // import "core-js/fn/array.find"
-  // ...
-export default class DummyClass {
+type Exceptions = Array<string>; 
 
+export default class Recase {
+  public static snakeCopy(obj: any, exceptions: Exceptions = []): any {
+    const recaseFunction = (key: any) => exceptions.includes(key) ? key : this.snakify(key);
+
+    return Recase.deepCopy(obj, recaseFunction);
+  }
+
+  public static camelCopy(obj: any, exceptions: Exceptions = []): any {
+    const recaseFunction = (key: any) => exceptions.includes(key) ? key : this.camelize(key);
+
+    return Recase.deepCopy(obj, recaseFunction);
+  }
+
+  public static camelize(key: any): any {
+    return key
+    .replace(/^(_+)/, '*$1*')
+    .replace(/_([a-z])/g, (_: any, c: any) => c.toUpperCase())
+    .replace(/^\*(_+)\*/, '$1');
+  }
+
+  public static snakify(key: any): any {
+    return (key.replace(/([A-Z])/g, '_$1').toLowerCase());
+  }
+
+  private static deepCopy(orig: any, recaseFunc: Function) {
+    switch (true) {
+      case Array.isArray(orig):
+        return Recase.recaseArray(orig, recaseFunc);
+      case Recase.isPresentObject(orig) && !Recase.isDate(orig):
+        return Recase.recaseObject(orig, recaseFunc);
+      default:
+        return orig;
+    }
+  }
+
+  private static recaseObject(orig: any, recaseFunc: Function) {
+    return Object.entries(orig).reduce((result: any, [key, value]) => {
+      const recasedKey = recaseFunc(key);
+      result[recasedKey] = Recase.deepCopy(value, recaseFunc);
+
+      return result;
+    }, {});
+  }
+
+  private static recaseArray(orig: any, recaseFunc: Function) {
+    return orig.map((val: any) => {
+      return Recase.deepCopy(val, recaseFunc);
+    });
+  }
+
+  private static isPresentObject(val: any) {
+    return val !== null && typeof val === 'object';
+  }
+
+  private static isDate(val: any) {
+    return val instanceof Date;
+  }
 }
--- test/recase.test.ts
+++ test/recase.test.ts
@@ -1,14 +1,119 @@
-import DummyClass from "../src/recase"
+import Recase from "../src/recase";
 
-/**
- * Dummy test
- */
-describe("Dummy test", () => {
-  it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+describe("Recase", () => {
+  describe('snakeCopy', () => {
+    describe('when given exceptions', () => {
+      it('ignores the exceptions', () => {
+        const object = {
+          randomIgnoredKey: true,
+          notIgnoredKey: false,
+          otherNotIgnoredKey: {
+            randomIgnoredKey: true,
+          }
+        };
 
-  it("DummyClass is instantiable", () => {
-    expect(new DummyClass()).toBeInstanceOf(DummyClass)
-  })
+        const result = Recase.snakeCopy(object, [
+          'randomIgnoredKey',
+        ]);
+
+        expect(result).toStrictEqual({
+          randomIgnoredKey: true,
+          not_ignored_key: false,
+          other_not_ignored_key: {
+            randomIgnoredKey: true,
+          }
+        });
+      });
+    });
+
+    describe('when given a date', () => {
+      it('returns the date', () => {
+        const date = new Date();
+
+        const result = Recase.snakeCopy(date);
+
+        expect(result).toStrictEqual(date);
+      });
+    });
+
+    describe('when given object', () => {
+
+      it('works', () => {
+        const result = Recase.snakeCopy({ camelCasedVariableName: true });
+
+        expect(result).toStrictEqual({
+          camel_cased_variable_name: true,
+        });
+      });
+    });
+
+    describe('when given an array', () => {
+      it('works', () => {
+        const result = Recase.snakeCopy([{ camelCasedVariableName: false }]);
+
+        expect(result).toStrictEqual([
+          {
+            camel_cased_variable_name: false,
+          }
+        ]);
+      });
+    });
+  });
+
+  describe('camelCopy', () => {
+    describe('when given exceptions', () => {
+      it('ignores the exceptions', () => {
+        const object = {
+          random_ignored_key: true,
+          not_ignored_key: false,
+          other_not_ignored_key: {
+            random_ignored_key: true,
+          }
+        };
+
+        const result = Recase.camelCopy(object, [
+          'random_ignored_key'
+        ]);
+
+        expect(result).toStrictEqual({
+          random_ignored_key: true,
+          notIgnoredKey: false,
+          otherNotIgnoredKey: {
+            random_ignored_key: true,
+          }
+        });
+      });
+    });
+
+    describe('when given a date', () => {
+      it('works', () => {
+
+        const date = new Date();
+
+        const result = Recase.snakeCopy(date);
+
+        expect(result).toStrictEqual(date);
+      });
+    });
+
+    describe('when given an object', () => {
+      it('works', () => {
+        const result = Recase.camelCopy({ snake_cased_variable_name: true });
+
+        expect(result).toStrictEqual({
+          snakeCasedVariableName: true,
+        });
+      });
+    });
+
+    describe('when given an array', () => {
+      it('works', () => {
+        const result = Recase.camelCopy([{ snake_cased_variable_name: true }]);
+
+        expect(result).toStrictEqual([{
+          snakeCasedVariableName: true,
+        }]);
+      });
+    });
+  });
 })
